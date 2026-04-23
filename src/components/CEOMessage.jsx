3306bc1067cc8f0ead36e4c13337e9cf17da7353
ShamiDesign <shami.design32@gmail.com>
2026-04-23 10:35:33 +0200

    update CEO image

--- src/components/CEOMessage.jsx
+++ src/components/CEOMessage.jsx
@@ -18,37 +18,34 @@ const CEOMessage = () => {
   }, []);
 
   return (
-    <section className=" flex flex-col md:flex-row  items-center gap-y-3 justify-between  bg-gray-100 px-5 md:px-12 lg:px-20 my-10 py-10  ">
-      <div data-aos="fade-up" className="Text flex flex-col w-full md:w-1/2">
-        <h1 className="text-[22px] md:text-[32px] font-semibold text-[var(--sec-color)] mb-3">
+    <section className=" flex flex-col md:flex-row  items-center gap-y-3 justify-between  bg-[var(--main-color)] px-5 md:px-12 lg:px-20 my-10 py-10  ">
+      <div data-aos="fade-up" className="Text flex flex-col  w-full ">
+        <h1 className="text-[22px] md:text-[32px] font-semibold text-[var(--sec-color)] mb-4 text-center">
           {t("ceoMessage.header")}
         </h1>
-        <h2 className="text-gray-600 font-light text-[12px] md:text-[14px] mb-3">
+        <h2 className="text-white/55 font-light text-[12px] md:text-[14px] mb-5 text-center">
           {t("ceoMessage.subtitle")}
         </h2>
         <div className="flex flex-col gap-2 w-full text-justify">
           {body.map((item, index) => (
-            <p className="text-[14px] md:text-[16px] leading-7" key={index}>
+            <p className="text-[14px] text-white md:text-[16px] leading-7 text-center" key={index}>
               {item}
             </p>
           ))}
         </div>
-        <h2 className="italic text-end font-semibold text-[15px] text-[18px] my-2">
+        <h2 className=" text-end text-white font-semibold text-[15px] text-[18px] my-2">
           {t("ceoMessage.signature")}
         </h2>
       </div>
 
-      <div
-       
-        className="Image rounded-2xl overflow-hidden bg-gray-100 shadow-lg "
-      >
+      {/* <div className="Image rounded-2xl overflow-hidden bg-gray-100 shadow-lg ">
         <img
-         data-aos="fade-right"
+          data-aos="fade-right"
           src={assets.CEO}
           alt=""
           className="w-[350px] md:w-[300px] lg:w-[400px]"
         />
-      </div>
+      </div> */}
     </section>
   );
 };
